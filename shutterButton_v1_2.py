279f8ebe2488e4389f7647386793cebf74c9f38f
Garrett Martin <garrett@standalone.co>
2016-04-07 10:39:49 -0400

    event listener bug fix

--- shutterButton_v1_2.py
+++ shutterButton_v1_2.py
@@ -102,9 +102,9 @@ def saturationCallback(self): 				#Control saturation adjustment attached to pus
 		saturationCount = 0 				#if saturation count already is 1, then this resets the count to 0
 		cameraSettings['saturation'] = 0 	#make saturation normal again
 		print "saturation is set to COLOR"
-	time.sleep(2)
-	eventListenSat()
-	#cameraReady()
+	#time.sleep(2)
+	#eventListenSat()
+	cameraReady()
 
 def snapPmode(self):
 	global imgCount 						#import global count
