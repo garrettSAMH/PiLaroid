7117cc0fc3721102c211b1221b90dfd6ba760d80
Garrett Martin <garrett@standalone.co>
2016-04-07 10:03:37 -0400

    Shutter Button Bug Fix

--- shutterButton_v1_2.py
+++ shutterButton_v1_2.py
@@ -152,7 +152,7 @@ def snapPmode(self):
 		camera.capture('/media/piCam/foo_'+date_string+'_{0:04}.jpg'.format(imgCount)) #Capture image, add the date into the file name and add the global count variable into file name. Everytime the program turns off then back on the count resets.
 		#camera.stop_preview() #stop preview
 	GPIO.output(35,False) 					#Turn off LED to signify end of image capture sequence
-	main()
+	cameraReady()
 
 
 
