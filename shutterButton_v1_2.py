900af7edd5f952f827ca1348268c8519db240226
Garrett Martin <garrett@standalone.co>
2016-04-07 16:32:02 -0400

    try shutter bug fix

--- shutterButton_v1_2.py
+++ shutterButton_v1_2.py
@@ -66,12 +66,12 @@ ShutterSpeedMicro = 0, 33333, 16666, 8000, 4000, 2000, 1000, 666, 500
 AWB = 'off', 'auto', 'sunlight', 'cloudy', 'shade', 'tungsten', 'fluorescent', 'incandescent', 'flash', 'horizon'
 
 def mainStart():
-	os.system("clear")
+	os.system("cls")
 	print "Hello World!"
 	print "PiLaroid v1.1"
 	print "Created by: Garrett Martin"
 	time.sleep(3)
-	os.system("clear")
+	os.system("cls")
 	cameraReady() 							#start the infinite loop function
 
 def cameraReady(): 							#idle loop keeping the program running while you do shit
